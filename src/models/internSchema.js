f3d1bf521dada63021368fc0e69be77f72dbc378
mahtoaman <itismyacnt@gmail.com>
2022-09-14 11:26:37 +0530

    internSchema created

---
+++ src/models/internSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require(mongoose);
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const internSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+
+    mobile: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+
+    collegeId: {
+      type: ObjectId,
+      required: true,
+      ref: "college",
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("intern", internSchema);
